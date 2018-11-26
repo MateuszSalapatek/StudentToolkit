907b17e6e5ec721b359e91e63b6d25a8e0f1079b
MateuszSalapatek <43645706+MateuszSalapatek@users.noreply.github.com>
2018-11-26 11:37:33 +0100

    removing unnecessary class PlaceholderFragment

--- app/src/main/java/com/example/matsal/studenttoolkit/DaySchedule.java
+++ app/src/main/java/com/example/matsal/studenttoolkit/DaySchedule.java
@@ -10,12 +10,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class DaySchedule extends AppCompatActivity {
 
@@ -62,7 +58,6 @@ public class DaySchedule extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         Log.d("function","DaySchedule.onCreateOptionsMenu");
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_day_schedule, menu);
         return true;
     }
@@ -70,60 +65,14 @@ public class DaySchedule extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Log.d("function","DaySchedule.onOptionsItemSelected");
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-            Log.d("function","PlaceholderFragment");
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            Log.d("function","PlaceholderFragment");
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            Log.d("function","onCreateView");
-            View rootView = inflater.inflate(R.layout.fragment_day_schedule, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -159,11 +108,11 @@ public class DaySchedule extends AppCompatActivity {
         public int getCount() {
             return pageCount;
         }
-
         @Override
         public CharSequence getPageTitle(int position) {
             Log.d("function","CharSequence");
             return "rest";
         }
+
     }
 }
